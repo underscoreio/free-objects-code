7ff801d7e45c3cc0e2c4f9521a0dc1a788d1e17a
Noel Welsh <noel@untyped.com>
2014-11-12 22:10:41 +0000

    Complete example
    List Option into the Free monad, provide two ways of executing the
    program. Can't really do anything more interesting with this example as
    the structure of the program doesn't expose any interesting content.

--- monad/src/main/scala/option/Option.scala
+++ monad/src/main/scala/option/Option.scala
@@ -1,5 +1,6 @@
 package option
 
+import scalaz.{~>, Free}
 import scalaz.std.option._
 
 object Examples {
@@ -9,7 +10,24 @@ object Examples {
       x <- Some(1)
       y <- Some(2)
       z <- Some(3)
-    }
+    } yield x + y + z
   }
 
+  def free = {
+    for {
+      x <- Free.liftF(some(1))
+      y <- Free.liftF(some(2))
+      z <- Free.liftF(some(3))
+    } yield x + y + z
+  }
+
+  val idExe: Option ~> Option = new (Option ~> Option) {
+    def apply[A](in: Option[A]): Option[A] =
+      in
+  }
+
+  val listExe: Option ~> List = new (Option ~> List) {
+    def apply[A](in: Option[A]): List[A] =
+      in map (List(_)) getOrElse Nil
+  }
 }
