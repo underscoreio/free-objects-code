fc4b2cf33bf93a271195bb9dd639282b167e4449
Noel Welsh <noel@untyped.com>
2014-11-13 16:54:59 +0000

    Timer example

--- monad/src/main/scala/option/Option.scala
+++ monad/src/main/scala/option/Option.scala
@@ -30,4 +30,26 @@ object Examples {
     def apply[A](in: Option[A]): List[A] =
       in map (List(_)) getOrElse Nil
   }
+
+  import scalaz.DList
+  final case class Timer[A](times: DList[Long], value: Option[A])
+  object Timer {
+    import scalaz.Monad
+
+    implicit val logMonad = new Monad[Timer] {
+      def bind[A, B](fa: Timer[A])(f: (A) ⇒ Timer[B]): Timer[B] = {
+        val now = System.nanoTime()
+        val result = fa.value.map(f).getOrElse(Timer(DList(), None))
+        Timer((fa.times :+ now) ++ result.times, result.value)
+      }
+      def point[A](a: ⇒ A): Timer[A] = {
+        Timer(DList(System.nanoTime()), some(a))
+      }
+    }
+  }
+
+  val timerExe: Option ~> Timer = new (Option ~> Timer) {
+    def apply[A](in: Option[A]): Timer[A] =
+      Timer(DList(), in)
+  }
 }
